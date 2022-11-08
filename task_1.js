38976924af85c37c460b86250202d6319391c1d4
LEX_DAY <lesha.dyatel@mail.ru>
2022-11-08 22:48:19 +0300

    change function TASK_2

--- task_1.js
+++ task_1.js
@@ -1,16 +1,24 @@
 'use strict';
 //TASK_1
-function TASK_1(){
+function TASK_1() {
     alert("Я JavaScript")
 }
 //TASK_2
 
 
-function TASK_2(){
-    
- document.forms.publish.onsubmit = function() {
-  let name, admin;
-  name = this.$name.value
-    admin = name; 
-    alert("Admin" + admin)
+function TASK_2() {
+    const inpt = document.getElementById('$name');
+    console.log(inpt)
+
+    let name, admin;
+
+    name = inpt.value;
+    admin = name;
+
+    alert("Admin:" + " " + admin)
+    inpt.value = ""
+}
+
+function prikol(event) {
+    console.log(event.target.value)
 }

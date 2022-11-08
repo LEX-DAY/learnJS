c4b8ec8243d405ea9d199fde6b6f9e36bf5e4c73
LEX_DAY <lesha.dyatel@mail.ru>
2022-11-08 19:58:06 +0300

    TASK_1
    TASK_2

--- task_1.js
+++ task_1.js
@@ -1,3 +1,16 @@
+'use strict';
+//TASK_1
 function TASK_1(){
     alert("Я JavaScript")
+}
+//TASK_2
+
+
+function TASK_2(){
+    
+ document.forms.publish.onsubmit = function() {
+  let name, admin;
+  name = this.$name.value
+    admin = name; 
+    alert("Admin" + admin)
 }

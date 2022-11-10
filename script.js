e6b24d240e9cac296577a37fa28f8030dba73850
LEX_DAY <lesha.dyatel@mail.ru>
2022-11-10 21:23:27 +0300

    Add Task TASK_9 logic

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ function TASK_2() {
 }*/
 
 // TASK_3
+/*
 let name = "Ilya";
 
 console.log("TASK_3")
@@ -63,7 +64,6 @@ true + false = 1
 null + 1 = 1
 undefined + 1 = NaN
 " \t \n" - 2 = -2 
-*/
 
 let firstNumber = prompt("Первое число?", 1);
 let secondNumber = prompt("Второе число?", 2);
@@ -80,44 +80,74 @@ undefined == null // true
 undefined === null // false
 null == "\n0\n" //false
 null === +"\n0\n" // false
-*/
+
 
 //TASK_8_1
 let nameJS = prompt('Какое "официальное" название JavaScript?', '');
 console.log("TASK_8_1")
 if (nameJS == 'ECMAScript') {
-  console.log('Верно!');
+    console.log('Верно!');
 } else {
-  console.log('Не знаете? ECMAScript!');
+    console.log('Не знаете? ECMAScript!');
 }
 
 //TASK_8_2
 let namberJS = prompt('Введите число', );
 console.log("TASK_8_2")
 if (namberJS > 0) {
-  console.log('Положительное число');
-}
-else if (namberJS < 0) {
-  console.log('Отрицательное число');
-}
-else if  (namberJS == 0) {
-  console.log('Нуль');
+    console.log('Положительное число');
+} else if (namberJS < 0) {
+    console.log('Отрицательное число');
+} else if (namberJS == 0) {
+    console.log('Нуль');
 } else {
-  console.log('Что-то ввели не так');
+    console.log('Что-то ввели не так');
 }
 
 //TASK_8_3
 let ac = prompt("Первое число?", 1);
 let ad = prompt("Второе число?", 2);
-let result = (+ac + +ad < 4) ? 'Мало':'Много';
+let result = (+ac + +ad < 4) ? 'Мало' : 'Много';
 console.log("TASK_8_3")
 console.log(result)
 
 //TASK_8_4
 let login = prompt('Введите логин', '');
 let massageLogin = (login == 'Сотрудник') ? 'Привет' :
-(login == 'Директор') ? 'Здравствуйте':'Нет логина';
+    (login == 'Директор') ? 'Здравствуйте' : 'Нет логина';
 console.log("TASK_8_4")
-console.log( massageLogin )
+console.log(massageLogin)
+*/
 
+//TASK_9_1
+function TASK_9() {
+    const inpt = document.getElementById('TASK_9');
+    console.log(inpt)
 
+    let age = Number(inpt.value);
+    if (age >= 14 && age <= 90)
+        alert("Я бы вам дал меньше лет");
+
+    else if (!(age < 14 && age > 90))
+        alert("Вы слишком юны или стары для этой жизни!")
+    else alert("Вы ввели что-то не то, попробуйте еще раз :)")
+}
+
+let nameLogin = prompt("Введите логин, пожалуйста", '');
+if (nameLogin === 'Админ') {
+    let password = prompt('Введите пароль, пожалуйста', '');
+
+    if (password === 'Я главный') {
+        alert('Здравствйте!')
+    } else if (password === '' || password === null) {
+        alert("Отменено")
+    } else {
+        alert("Неверный пароль, попробуйте еще!")
+    }
+}
+    
+    else if (nameLogin === '' || nameLogin === null) {
+        alert("Отменено")
+    } else {
+        alert("Кто вы??")
+    }
